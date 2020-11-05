dc27666ac77e66a3a2a9448efc031585d8778a56
maini <philip.mattsson@iths.se>
2020-11-05 10:53:53 +0100

    Third commit, with new feature

--- src/com/Mattsson/Main.java
+++ src/com/Mattsson/Main.java
@@ -3,6 +3,6 @@ package com.Mattsson;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Second version");
+        System.out.println("Third version, with new feature");
     }
 }
